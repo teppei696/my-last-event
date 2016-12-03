7a11d2040ae6b3b4984fb989eec3f127a89ceba5
teppei696 <teppei6969@gmail.com>
2016-12-03 22:56:16 +0900

    13

--- callback.php
+++ callback.php
@@ -3,6 +3,8 @@
 	use YConnect\Credential\ClientCredential;
 	use YConnect\YConnectClient;
 
+  var $size,$iv,$enc_token,$base64_token;
+  
 	// アプリケーションID, シークレット
 	$client_id = "dj0zaiZpPURVTVRhTThhaUpaSiZzPWNvbnN1bWVyc2VjcmV0Jng9NmI-";
 	$client_secret = "3888f419905d859111f2640028cdcd84e269d530";
