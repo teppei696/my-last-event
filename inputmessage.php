e5db1fa40faa75770311b0f107e66e8fecac8c59
teppei696 <teppei6969@gmail.com>
2016-12-04 13:08:12 +0900

    JS対応が漏れていた

--- inputmessage.php
+++ inputmessage.php
@@ -339,10 +339,10 @@
     </div>
 
     <!-- jQuery -->
-    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="bootstrap.min.js"></script>
 
     <!-- Plugin JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
